edc0b665313bd1803c9d7b649ac796aeefe7087a
howddy <mail.howddy@gmail.com>
2023-07-06 12:26:20 +0530

    Updated testNG.xml

--- src/test/java/com/example/HowddyStaticHtmlFormDemo/TC001.java
+++ src/test/java/com/example/HowddyStaticHtmlFormDemo/TC001.java
@@ -1,65 +1,169 @@
-package com.HowddyStaticHtmlFormDemo;
+package com.example.HowddyStaticHtmlFormDemo;
 //package com.example.HowddyStaticHtmlForm;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import utils.listeners.retry;
+import utils.listeners.testNGListener;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 
 import java.util.concurrent.TimeUnit;
 
+@Listeners(testNGListener.class)
 public class TC001 {
-    private WebDriver driver;
+    //private WebDriver driver;
+
     private String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
     private JavascriptExecutor js;
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<RemoteWebDriver>();
+    public static String remote_url = "http://192.168.1.5:4444/";
+    public Capabilities capabilities;
 
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        baseUrl = "https://formy-project.herokuapp.com/form";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        js = (JavascriptExecutor) driver;
+    private static final Logger logger = LoggerFactory.getLogger(com.example.HowddyStaticHtmlForm.TC001.class);
+
+    private static final String FILE_PATH = "src/test/resources/testData/testdata.xlsx";
+
+    private static ExtentReports extent;
+    private static ExtentTest test;
+
+    @BeforeSuite
+    public void before() {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("src/test/java/utils/testOutput/extent-report.html");
+        htmlReporter.config().setTheme(Theme.DARK);
+
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
     }
 
-    @Test
-    public void testTC001() throws Exception {
-        driver.get(baseUrl);
-        setFirstName("John");
-        setLastName("Amber");
-        setJobTitle("Tester");
-        selectRadioButton("radio-button-2");
-        selectCheckbox("checkbox-1");
-        selectOptionFromMenu("select-menu", "2-4");
-        selectDate("datepicker", "06/28/2023");
+    @Parameters({"browser"})
+    @BeforeClass(alwaysRun = true)
+        public void setUp(String browser) throws Exception {
+
+            Properties prop = new Properties();
+            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            prop.load(fis);
+
+        //System.setProperty("webdriver.chrome.driver", prop.getProperty("webdriver.chrome.driver"));
+
+        //Using selenium grid
+        //Set the web drivers path to System Variable
+        //Go to the location of "selenium-server-4.10.0.jar"
+        //run the command "java -jar selenium-server-4.10.0.jar standalone"
+
+        System.out.println("Test is running on "+ browser);
+        logger.info("Test is running on "+ browser);
+        if(browser.equals("firefox")) {
+            capabilities = new FirefoxOptions();
+        } else if (browser.equals("chrome")) {
+            capabilities = new ChromeOptions();
+        }
+
+
+
+        driver.set(new RemoteWebDriver(new URL(remote_url), capabilities));
+        baseUrl = prop.getProperty("baseUrl");
+        driver.get().get(baseUrl);
+        driver.get().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //js = (JavascriptExecutor) driver;
+    }
+
+    @Test(dataProvider = "formData", retryAnalyzer = retry.class)
+    public void testTC001(String firstName, String lastName, String jobTitle, String radioButtonId,
+                          String checkboxId, String selectMenuId, String selectOption, String datePickerId,
+                          String date) throws Exception {
+        driver.get().get(baseUrl);
+        System.out.println(firstName+lastName+jobTitle+radioButtonId+checkboxId+selectMenuId+selectOption+datePickerId+date);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setJobTitle(jobTitle);
+        selectRadioButton(radioButtonId);
+        selectCheckbox(checkboxId);
+        selectOptionFromMenu(selectMenuId, selectOption);
+        selectDate(datePickerId, date);
         clickSubmitButton();
+
+        test = extent.createTest("Test Case: TC001");
+        test.log(Status.INFO, "Test data: " + firstName + ", " + lastName + ", " + jobTitle +
+                ", " + radioButtonId + ", " + checkboxId + ", " + selectMenuId + ", " +
+                selectOption + ", " + datePickerId + ", " + date);
 
         try {
             Assert.assertEquals(getSuccessMessage(), "The form was successfully submitted!");
+            logger.info("The form was successfully submitted!");
+            System.out.println("Form Successfully Submitted");
         } catch (Error e) {
             verificationErrors.append(e.toString());
+            test.log(Status.FAIL, "Form submission failed");
+            test.log(Status.ERROR, e);
         }
     }
 
+    @DataProvider(name = "formData")
+    public Object[][] getFormData() throws IOException {
+        FileInputStream fis = new FileInputStream(FILE_PATH);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0);
+//        Sheet sheet = workbook.getSheet("Sheet1");
+        int rowCount = sheet.getLastRowNum();
+        int colCount = sheet.getRow(0).getLastCellNum();
+
+        Object[][] data = new Object[rowCount][colCount];
+
+        for (int i = 1; i <= rowCount; i++) {
+            Row row = sheet.getRow(i);
+            for (int j = 0; j < colCount; j++) {
+                Cell cell = row.getCell(j);
+                data[i - 1][j] = cell.getStringCellValue();
+            }
+        }
+
+
+        workbook.close();
+        fis.close();
+
+        return data;
+    }
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
-        driver.quit();
+        driver.get().quit();
+        driver.remove();
         String verificationErrorString = verificationErrors.toString();
         if (!verificationErrorString.isEmpty()) {
             Assert.fail(verificationErrorString);
         }
     }
 
+    @AfterSuite
+    public void tearDownSuite() {
+        extent.flush();
+    }
+
     private WebElement findElement(By by) {
-        return driver.findElement(by);
+        return driver.get().findElement(by);
     }
 
     private void clickElement(By by) {
@@ -76,7 +180,7 @@ public class TC001 {
 
     private boolean isElementPresent(By by) {
         try {
-            driver.findElement(by);
+            driver.get().findElement(by);
             return true;
         } catch (org.openqa.selenium.NoSuchElementException e) {
             return false;
@@ -85,7 +189,7 @@ public class TC001 {
 
     private boolean isAlertPresent() {
         try {
-            driver.switchTo().alert();
+            driver.get().switchTo().alert();
             return true;
         } catch (org.openqa.selenium.NoAlertPresentException e) {
             return false;
@@ -94,7 +198,7 @@ public class TC001 {
 
     private String closeAlertAndGetText() {
         try {
-            org.openqa.selenium.Alert alert = driver.switchTo().alert();
+            org.openqa.selenium.Alert alert = driver.get().switchTo().alert();
             String alertText = alert.getText();
             if (acceptNextAlert) {
                 alert.accept();
